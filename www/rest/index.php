d8dd01e55489aceebc2c40cd2dfe16a187853a84
Peter Serwylo <peter@serwylo.com>
2015-07-04 21:18:44 +1000

    Added REST end point for battle. Also renamed attributes to lowercase camel case.

--- www/rest/index.php
+++ www/rest/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Jacwright\RestServer\RestException;
 use Jacwright\RestServer\RestServer;
 
 require 'vendor/autoload.php';
@@ -12,6 +13,7 @@ $server = new RestServer($mode);
 
 $server->addClass('LgaController');
 $server->addClass('AttributeController');
+$server->addClass('BattleController');
 
 $server->handle();
 
@@ -68,7 +70,118 @@ class LgaController extends DataAccess {
 
 }
 
+class BattleController extends DataAccess {
+
+	/**
+	 * @url GET /battle/$winnerId/$loserId
+	 * @param int $winnerId
+	 * @param int $loserId
+	 * @return array[]
+	 * @throws RestException
+	 */
+	public function battle( $winnerId, $loserId ) {
+
+		$lga = new LgaController();
+		$winner = $lga->get( $winnerId );
+		$loser  = $lga->get( $loserId  );
+
+		if ( !$winner ) {
+			throw new RestException( 404, "LGA $winnerId not found." );
+		}
+
+		if ( !$loser ) {
+			throw new RestException( 404, "LGA $loserId not found." );
+		}
+
+		$attributesLoader = new AttributeController();
+		$attributes = $attributesLoader->listAll();
+
+		$winnerAttributes = $this->attributesFor( $winnerId );
+		$loserAttributes  = $this->attributesFor( $loserId  );
+
+		$winner[ 'attributes' ] = array();
+		$loser [ 'attributes' ] = array();
+
+		foreach( $attributes as $attribute ) {
+
+			$attributeId = $attribute[ 'attributeId' ];
+			$winnerValue = array_key_exists( $attributeId, $winnerAttributes ) ? $winnerAttributes[ $attributeId ] : null;
+			$loserValue  = array_key_exists( $attributeId, $loserAttributes  ) ? $loserAttributes [ $attributeId ] : null;
+
+			if ( !$winnerValue || !$loserValue ) {
+				continue;
+			}
+
+			$result = $attributesLoader->compare( $winnerValue, $loserValue, $attribute );
+
+			if ( $result === AttributeController::BETTER ) {
+				$winner[ 'attributes' ][ $attributeId ] = $winnerValue;
+				$loser [ 'attributes' ][ $attributeId ] = $loserValue;
+			}
+		}
+
+		return array(
+			'winner'     => $winner,
+			'loser'      => $loser,
+			'attributes' => $attributes,
+		);
+
+	}
+
+	/**
+	 * @param $lgaId
+	 * @return array Key is the attribute id, and the value is the attribute value.
+	 */
+	private function attributesFor( $lgaId ) {
+
+		$attributes = $this->select(
+			<<<SQL
+SELECT attributeId, attributeValue
+FROM attributeValues
+WHERE lgaId = :lgaId
+SQL
+			, array( ':lgaId' => $lgaId )
+		);
+
+		$results = array();
+		foreach ( $attributes as $row ) {
+			$results[ $row[ 'attributeId' ] ] = $row[ 'attributeValue' ];
+		}
+		return $results;
+	}
+
+}
+
 class AttributeController extends DataAccess {
+
+	const BETTER = 1;
+	const WORSE  = -1;
+	const EQUAL  = 0;
+
+	public function compare( $valueOne, $valueTwo, array $attribute ) {
+
+		$compare = $attribute[ 'comparator' ];
+		if ( $compare == 'higher' || !$compare ) { // TODO: Remove the !$compare, should have higher or lower value instead.
+			if ( $valueOne > $valueTwo  ) {
+				return self::BETTER;
+			} else if ( $valueTwo > $valueOne ) {
+				return self::WORSE;
+			} else {
+				return self::EQUAL;
+			}
+		} else if ( $compare == 'lower' ) {
+			if ( $valueOne < $valueTwo ) {
+				return self::BETTER;
+			} else if ( $valueTwo < $valueOne ) {
+				return self::WORSE;
+			} else {
+				return self::EQUAL;
+			}
+		} else {
+			throw new RestException( "500", "Comaprator for attribute {$attribute['AttributeName']} should be 'lower' or 'higher', but it was '$compare'.'" );
+		}
+
+	}
 
 	/**
 	 * @url GET /attributes/list
@@ -77,19 +190,19 @@ class AttributeController extends DataAccess {
 	public function listAll() {
 		return $this->select( <<<SQL
 SELECT
-	AttributeID,
-	AttributeName,
-	AttributeDescription,
-	CategoryID,
-	CategoryName,
-	PositivePhrase,
-	NegativePhrase,
-	Comparator,
-	Denominator,
-	DenominatorRationale,
-	DataSourceLink,
-	DataSourceRationale,
-	DataSourceDate
+	attributeId,
+	attributeName,
+	attributeDescription,
+	categoryId,
+	categoryName,
+	positivePhrase,
+	negativePhrase,
+	comparator,
+	denominator,
+	denominatorRationale,
+	dataSourceLink,
+	dataSourceRationale,
+	dataSourceDate
 FROM
 	attributemetadata
 SQL
