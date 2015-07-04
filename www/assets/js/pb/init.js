1bb685b7d5c32552026b94ccc193d52f56462aa8
Daniel Sun <hjdaniel.sun@gmail.com>
2015-07-05 09:48:43 +1000

    switch env to live

--- www/assets/js/pb/init.js
+++ www/assets/js/pb/init.js
@@ -2,4 +2,4 @@ var PB = PB || {};
 PB.HASH_URL_TEMPLATE = "#why-is-{good_key}-better-than-{bad_key}";
 PB.Attributes = [];
 PB.LGAs = [];
-PB.env = "dev"; // dev or live
+PB.env = "live"; // dev or live
