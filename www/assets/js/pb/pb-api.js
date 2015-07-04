4e89132297e8e6fdb0de90f01ed397b5160bdc07
Daniel Sun <hjdaniel.sun@gmail.com>
2015-07-04 23:52:27 +1000

    low level api wrapping js

--- www/assets/js/pb/pb-api.js
+++ www/assets/js/pb/pb-api.js
@@ -1,8 +1,36 @@
+// low level lga rest API wrapping.
 PB.api = ( function ( module, $ ) {
-	module.getAttributes = function() {
-		var ajaxOptions = {};
-		return $.ajax(ajaxOptions);
-	};
+  var initAjaxOptions = function () {
+    return { dataType: 'json', contentType: 'application/json'};	
+  };
+  
+  module.getAutorityNameHint = function ( text ) {
+    var ajaxOptions = initAjaxOptions();
+    ajaxOptions.url = "/rest/lga/search/" + text;
+    ajaxOptions.type = "GET";
+    return $.ajax(ajaxOptions);
+  };
 	
-	return module;
+  module.getLocalAuthorities = function () {
+    var ajaxOptions = initAjaxOptions();
+    ajaxOptions.url = "/rest/lga/list";
+    ajaxOptions.type = "GET";
+    return $.ajax(ajaxOptions);
+  };
+  
+  module.getAttributes = function () {
+    var ajaxOptions = initAjaxOptions();
+    ajaxOptions.url = "/rest/attributes/list";
+    ajaxOptions.type = "GET";
+    return $.ajax(ajaxOptions);
+  };
+  
+  module.getLocalAuthorityById = function ( localAuthorityId ) {
+    var ajaxOptions = initAjaxOptions();
+    ajaxOptions.url = "/rest/lga/" + localAuthorityId.toString();
+    ajaxOptions.type = "GET";
+    return $.ajax(ajaxOptions);
+  };
+  
+  return module;
 })( PB.api || {}, $ );
