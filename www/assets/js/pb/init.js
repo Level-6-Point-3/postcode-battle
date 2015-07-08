70c85a72cc28fafb4f2318ce620251f9e64d28dc
Daniel Sun <hjdaniel.sun@gmail.com>
2015-07-09 01:25:36 +1000

    use sammyjs routing

--- www/assets/js/pb/init.js
+++ www/assets/js/pb/init.js
@@ -1,10 +1,10 @@
 var PB = PB || {};
 PB.HASH_URL_TEMPLATE = "#why-is-{good_key}-better-than-{bad_key}";
 PB.URL_FRAGMENTS = {
-    RUN_BATTLE       : /^\#why-is-(.+)-better-than-(.+)/,
-    SHOW_LIVEABILITY : /^\#live/,
-    WHO_WE_ARE       : /^\#about/,
-    SHOW_BATTLE      : /^$/
+    SHOW_LIVEABILITY : "#live",
+    WHO_WE_ARE       : "#about",
+    SHOW_BATTLE      : "#battle",
+    RUN_BATTLE       : /\#why\-is\-(.+)\-better\-than\-(.+)/
 };
 PB.Attributes = [];
 PB.LGAs = [];
