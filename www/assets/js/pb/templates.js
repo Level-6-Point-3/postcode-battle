4756c1d41f220dfe86c224f551a309ff49b33910
Daniel Sun <hjdaniel.sun@gmail.com>
2015-07-19 17:35:46 +1000

    dynamically parse facebook integration

--- www/assets/js/pb/templates.js
+++ www/assets/js/pb/templates.js
@@ -1,37 +1,45 @@
-PB.templates = ( function ( module, Handlebars, $ ){
+PB.templates = ( function ( module, Handlebars, $ ) {
 	module.attributeTemplate = function( itemHeading, attributeLabel, isWinner ) {
-		var source = $("#attribute-template").html();
-		var template = Handlebars.compile( source );
+		var source = $("#attribute-template").html(),
+			template = Handlebars.compile( source );
+
 		return template( { itemHeading: itemHeading, label: attributeLabel, winner: isWinner } );
 	};
 	
 	module.battleFieldStartTemplate = function () {
-		var source = $("#battle-field-start-template").html();
-		var template = Handlebars.compile( source );
+		var source = $("#battle-field-start-template").html(),
+			template = Handlebars.compile( source );
 		
 		return template();	
 	};
 	
 	module.battleFieldResultTemplate = function () {
-		var source = $("#battle-field-result-template").html();
-		var template = Handlebars.compile( source );
+		var source = $("#battle-field-result-template").html(),
+			template = Handlebars.compile( source );
 		
 		return template();	
 	};
 
 	module.whoAreWeTemplate = function () {
-		var source = $("#who-are-we-template").html();
-		var template = Handlebars.compile( source );
+		var source = $("#who-are-we-template").html(),
+			template = Handlebars.compile( source );
 
 		return template();
 	};
 
-    module.liveabilityIndexTemplate = function(){
-        var source =$("#liveability-index-template").html();
-        var template = Handlebars.compile(source);
+    module.liveabilityIndexTemplate = function() {
+        var source =$("#liveability-index-template").html(),
+        	template = Handlebars.compile(source);
 
         return template();
     };
+	
+	module.socialMedia.facebookTemplate = function() {
+		var source=$("#facebook-integration-template").html(),
+			template = Handlebars.compile(source);
+		
+		return template();
+	};
 
 	return module;
 } )( PB.templates || {}, Handlebars || {}, $ );
