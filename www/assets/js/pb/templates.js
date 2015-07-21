91ae1833df0fc13c500486c6a62a1944e81f1381
Daniel Sun <hjdaniel.sun@gmail.com>
2015-07-21 10:57:19 +1000

    critical bug fix...

--- www/assets/js/pb/templates.js
+++ www/assets/js/pb/templates.js
@@ -1,45 +1,47 @@
-PB.templates = ( function ( module, Handlebars, $ ) {
-	module.attributeTemplate = function( itemHeading, attributeLabel, isWinner ) {
+PB.templates = (function (module, Handlebars, $) {
+	module.attributeTemplate = function (itemHeading, attributeLabel, isWinner) {
 		var source = $("#attribute-template").html(),
-			template = Handlebars.compile( source );
+			template = Handlebars.compile(source);
 
-		return template( { itemHeading: itemHeading, label: attributeLabel, winner: isWinner } );
+		return template({ itemHeading: itemHeading, label: attributeLabel, winner: isWinner });
 	};
-	
+
 	module.battleFieldStartTemplate = function () {
 		var source = $("#battle-field-start-template").html(),
-			template = Handlebars.compile( source );
-		
-		return template();	
+			template = Handlebars.compile(source);
+
+		return template();
 	};
-	
+
 	module.battleFieldResultTemplate = function () {
 		var source = $("#battle-field-result-template").html(),
-			template = Handlebars.compile( source );
-		
-		return template();	
+			template = Handlebars.compile(source);
+
+		return template();
 	};
 
 	module.whoAreWeTemplate = function () {
 		var source = $("#who-are-we-template").html(),
-			template = Handlebars.compile( source );
+			template = Handlebars.compile(source);
 
 		return template();
 	};
 
-    module.liveabilityIndexTemplate = function() {
-        var source =$("#liveability-index-template").html(),
-        	template = Handlebars.compile(source);
+    module.liveabilityIndexTemplate = function () {
+        var source = $("#liveability-index-template").html(),
+			template = Handlebars.compile(source);
 
         return template();
     };
-	
-	module.socialMedia.facebookTemplate = function() {
-		var source=$("#facebook-integration-template").html(),
-			template = Handlebars.compile(source);
-		
-		return template();
-	};
+
+	module.socialMedia = {
+		facebookTemplate: function () {
+			var source = $("#facebook-integration-template").html(),
+				template = Handlebars.compile(source);
+
+			return template();
+		}
+	}
 
 	return module;
-} )( PB.templates || {}, Handlebars || {}, $ );
+})(PB.templates || {}, Handlebars || {}, $);
